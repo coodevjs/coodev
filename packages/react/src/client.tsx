b8b1cd9ee8f45214af5f747e9cad741c6e7cb754
wokeyi <wokeyifrontend@gmail.com>
2023-04-29 10:44:40 +0900

    fix: get initial props on csr

--- packages/react/src/client.tsx
+++ packages/react/src/client.tsx
@@ -4,38 +4,46 @@ import type { GlobalData } from './types'
 import routes from '__COODEV__/react/routes'
 import coodevConfig from '__COODEV__/react/config'
 import CoodevApp from './components/CoodevApp'
+import App from '__COODEV__/react/app'
 import { COODEV_APP_ID, COODEV_DATA_ID } from './constants'
 import { findMatchedRoute } from './utils'
+;(async () => {
+  const url = location.pathname + location.search
 
-let globalData: GlobalData = {
-  pageProps: {},
-}
+  const matched = findMatchedRoute(url, routes) || {
+    component: null,
+    path: null,
+  }
 
-const scriptElement = document.getElementById(COODEV_DATA_ID)
-if (scriptElement) {
-  globalData = JSON.parse(scriptElement!.innerText)
-}
+  let globalData: GlobalData = {
+    pageProps: {},
+  }
 
-const url = location.pathname + location.search
+  if (coodevConfig.ssr) {
+    const scriptElement = document.getElementById(COODEV_DATA_ID)
+    if (scriptElement) {
+      globalData = JSON.parse(scriptElement!.innerText)
+    }
+  } else if (App.getInitialProps) {
+    globalData.pageProps = await App.getInitialProps({
+      Component: matched.component,
+    })
+  }
 
-const matched = findMatchedRoute(url, routes) || {
-  component: null,
-  path: null,
-}
+  const content = (
+    <CoodevApp
+      url={url}
+      path={matched.path}
+      Component={matched.component}
+      pageProps={globalData.pageProps || {}}
+    />
+  )
 
-const content = (
-  <CoodevApp
-    url={url}
-    path={matched.path}
-    Component={matched.component}
-    pageProps={globalData.pageProps || {}}
-  />
-)
+  const container = document.getElementById(COODEV_APP_ID) as HTMLElement
 
-const container = document.getElementById(COODEV_APP_ID) as HTMLElement
-
-if (coodevConfig.ssr) {
-  ReactDOM.hydrateRoot(container, content)
-} else {
-  ReactDOM.createRoot(container).render(content)
-}
+  if (coodevConfig.ssr) {
+    ReactDOM.hydrateRoot(container, content)
+  } else {
+    ReactDOM.createRoot(container).render(content)
+  }
+})()
